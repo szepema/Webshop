0f84fa85e65041ed5e9bea0e32e1b895dbc84cc5
csokapeter <55291376+csokapeter@users.noreply.github.com>
2021-11-20 16:56:34 +0100

    Delete utils.php

--- src/includes/utils.php
+++
@@ -1,5 +0,0 @@
-<?php
-    function atalakitas_link($sz){
-        return str_replace([',', ' ', 'á', 'é', 'í', 'ó', 'ö', 'ő', 'ú', 'ü', 'ű'],['', '-', 'a', 'e', 'i', 'o', 'o', 'o', 'u', 'u', 'u'],strtolower($sz));
-    }
-?>
